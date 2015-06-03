239f25a6430739a0bd8a4576ff8d3769e1359750
arturro <artchru@gmail.com>
2015-06-03 11:29:41 +0200

    Site updated to

--- javascripts/all.js
+++ javascripts/all.js
@@ -1159,3 +1159,14 @@ $( function() {
 
 
 
+
+$(document).ready(function() {
+
+  var docHeight = $(window).height();
+  var footerHeight = $('footer').height();
+  var footerTop = $('footer').position().top + footerHeight;
+
+  if (footerTop < docHeight) {
+    $('footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+  }
+});
